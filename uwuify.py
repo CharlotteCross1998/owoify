0c1879f47e3b4cdf8ab5cec67a9b8cde0c07076b
Charlotte <29734551+CharlotteCross1998@users.noreply.github.com>
2023-01-23 17:04:17 +0000

    Added function for language specific edits
    Added inc function for language specific edits. Also changed the copy of file to happen first.

--- uwuify.py
+++ uwuify.py
@@ -10,54 +10,58 @@ def find_between( s, first, last ):
     except ValueError:
         return ""
 
+def editInc(line):
+    excludeLines = [".include", "%MACRO", ".incbin"]
+    #excludes anything that shouldn't be owo'ified, such as include headers
+    for exclude in excludeLines:
+        if exclude in line:
+            return line
+        continue
+
+    #don't try and owo'ify if there's not even a string there
+    if '\"' in line:
+        #Make a copy of the line
+        newcontent = line
+        
+        #get everything between speech marks 
+        uwu = find_between(newcontent, '"', '"')
+        #get everything between { and } from the original
+        dontreplace = find_between(newcontent, '{', '}')
+        #remove anything between { and }
+        uwu = uwu.replace(dontreplace, "")
+        #owoify it
+        owo = owoify(uwu)
+        #put back anything between { and }
+        owo = owo.replace("{}", "{" + dontreplace + "}")
+                        
+        #replace non-owo with owo
+        newcontent = line.replace("\""+ uwu + "\"", "\"" + owo + "\"")
+        return newcontent.replace(uwu, owo)
+    else:
+        #Nothing to edit, keep as original so add to edited file contents
+        return line
+    
+
 #Function to edit a file given a name
-def editFile(filename):
-    newFileContents = ""
-    excludeLines = [".include", "#include", "%MACRO", ".incbin"]
-    #Open the file in readonly mode so can edit it later
-    with open(filename, 'r') as file:
-        #Go through file line by line
-        for line in file:
-            #excludes anything that shouldn't be owo'ified, such as include headers
-            owo = False
-            for exclude in excludeLines:
-                if exclude in line:
-                    owo = True
-                    break
-            if owo:
-                newFileContents += line
-                continue
-                
-            #don't try and owo'ify if there's not even a string there
-            if '\"' in line:
-                #Make a copy of the line
-                newcontent = line
-
-                #get everything between speech marks 
-                uwu = find_between(newcontent, '"', '"')
-                #get everything between { and } from the original
-                dontreplace = find_between(newcontent, '{', '}')
-                #remove anything between { and }
-                uwu = uwu.replace(dontreplace, "")
-                #owoify it
-                owo = owoify(uwu)
-                #put back anything between { and }
-                owo = owo.replace("{}", "{" + dontreplace + "}")
-                
-                #replace non-owo with owo
-                newcontent = line.replace("\""+ uwu + "\"", "\"" + owo + "\"")
-                
-                newFileContents += newcontent.replace(uwu, owo)
-            else:
-                #Nothing to edit, keep as original so add to edited file contents
-                newFileContents += line
-
+def editFile(filename, ext):
     #Make a copy of the file in case anything goes wrong
-
     #get the extension by splitting the filename into parts, reversing it and getting first element
     ext = os.path.splitext(filename)[-1:][0]
     editedFilename = filename.replace(ext, ".bak")
     shutil.copy2(filename, editedFilename)
+
+    newFileContents = ""
+    #Open the file in readonly mode so can edit it later
+    with open(filename, 'r') as file:
+        #Go through file line by line
+        for line in file:
+            #Language specific edits
+            if ext == ".inc":
+                newFileContents += editInc(line)
+            else:
+                #TODO: Add verbose output?
+                print("Skipping " + filename + " as it's not supported")
+                pass
     #Write edited data to file
     with open(filename, 'w') as file:
         file.write(newFileContents)
@@ -70,7 +74,8 @@ print("Starting...")
 
 #If single source file is specified, edit only that
 if args["file"] is not None:
-    editFile(args["file"])
+    ext = os.path.splitext(args["file"])[-1:][0]
+    editFile(args["file"], ext)
 #Otherwise, go through all files in current directory 
 #If files has compatible extension, then edit them
 else:
@@ -80,6 +85,6 @@ else:
         for file in files:
             for ext in fileExts:
                 if file.endswith("." + ext):
-                    editFile(os.path.join(root, file))
+                    editFile(os.path.join(root, file), "." + ext)
 
 print("Finished")
